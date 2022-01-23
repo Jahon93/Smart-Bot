0c9948e8c630d3334e3f1ff75384143aaa76eccf
Jakhongir <iskandarovjahongir@gmail.com>
2022-01-23 22:28:11 +0900

    updated

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("get-activity").addEventListener("click", function() {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("activity").textContent = data.activity
+      document.getElementById("title").textContent = "Plan to ..."
+      document.body.classList.add("fun")
+    })
+})
